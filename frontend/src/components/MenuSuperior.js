546deae8a4e24041d78f6c0acbbd2902bdb4b0ea
Javier Devoto <javierdevoto96@gmail.com>
2019-06-04 01:07:39 -0300

    push final de la noche de este branch

--- frontend/src/components/MenuSuperior.js
+++ frontend/src/components/MenuSuperior.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-
-function TabContainer(props) {
-  return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
-      {props.children}
-    </Typography>
-  );
-}
-
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 const useStyles = makeStyles(theme => ({
   root: {
