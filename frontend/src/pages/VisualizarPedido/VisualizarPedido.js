300d5e55f1641b48ae2005594a9e11a5d9d33a06
Lautaro <lautaro@lautaro.com>
2019-06-07 23:01:06 -0300

    se ve el pedido. faltan boton pedir cuenta y jugar juego

--- frontend/src/pages/VisualizarPedido/VisualizarPedido.js
+++ frontend/src/pages/VisualizarPedido/VisualizarPedido.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import List from '@material-ui/core/List';
 import ItemPedido from './ItemPedido/ItemPedido';
-import { Typography, Card, CardContent } from '@material-ui/core';
+import { Typography, Card, CardContent, Paper } from '@material-ui/core';
 import { ServiceLocator } from "../../services/ServiceLocator.js";
 import MenuInferior from '../../components/menuInferior/MenuInferior';
+import './VisualizarPedido.scss';
 
 export default class VisualizarPedido extends Component {
   constructor(props) {
@@ -30,6 +31,11 @@ export default class VisualizarPedido extends Component {
     this.props.history.push('/carta')
   }
 
+  getPrecioTotal() {
+    return this.state.pedidos.map((pedido) => pedido.cantidad * pedido.itemCarta.precioUnitario)
+      .reduce((a, b) => a + b)
+  }
+
   render() {
     const { pedidos } = this.state;
 
@@ -46,6 +52,14 @@ export default class VisualizarPedido extends Component {
             return <ItemPedido key={pedido.id} pedido={pedido} />
           })}
         </List>
+        <Paper>
+          <Typography className="precioFinal" variant="subtitle1">
+            Precio final: {new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            }).format(this.getPrecioTotal())}
+          </Typography>
+        </Paper>
         <MenuInferior handlers={{ onChange: this.verCarta }} boton="VER CARTA" />
       </div>
     )
