c2d590d3129afb12ea556cd204825274c9ca1272
Javier Devoto <javierdevoto96@gmail.com>
2019-07-03 19:09:36 -0300

    Un par de fixes

--- frontend/src/pages/CambiarContraseña/CambiarContraseña.js
+++ frontend/src/pages/CambiarContraseña/CambiarContraseña.js
@@ -9,126 +9,129 @@ import { ControllerDeEmpleado } from '../../controller/ControllerDeEmpleado';
 
 export default class CambiarContraseña extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            contraseñaActual: "",
-            contraseñaNueva: "",
-            confirmarContraseñaNueva: "",
-            open: false,
-            errorMessage: ""
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      contraseñaActual: "",
+      contraseñaNueva: "",
+      confirmarContraseñaNueva: "",
+      open: false,
+      errorMessage: ""
     }
+  }
 
-    handleEnviar = () => {
-        const { contraseñaActual, contraseñaNueva, confirmarContraseñaNueva } = this.state
-        if(contraseñaNueva === confirmarContraseñaNueva) {
-            const json = {
-                idEmpleado: ControllerDeEmpleado.getSesionActiva(),
-                contraseñaActual: contraseñaActual,
-                contraseñaNueva: contraseñaNueva,
+  handleEnviar = () => {
+    const { contraseñaActual, contraseñaNueva, confirmarContraseñaNueva } = this.state
+    if (contraseñaActual === contraseñaNueva) {
+      this.generarError("Contraseña nueva igual a la actual")
+    } else {
+      if (contraseñaNueva === confirmarContraseñaNueva) {
+        const json = {
+          idEmpleado: ControllerDeEmpleado.getSesionActiva(),
+          contraseñaActual: contraseñaActual,
+          contraseñaNueva: contraseñaNueva,
+        }
+        ServiceLocator.EmpleadoService.cambiarContraseña(json)
+          .then((respuesta) => {
+            if (!respuesta.error) {
+              this.handleClickOpen()
+            } else {
+              this.generarError(respuesta.error.response.data.error)
             }
-            ServiceLocator.EmpleadoService.cambiarContraseña(json)
-                .then((respuesta) => {
-                    const error = respuesta.error.response.data.error
-                    if (!error) {
-                        this.handleClickOpen()
-                    } else {
-                        this.generarError(error)
-                    }
-                })
-        } else {
-            this.generarError("Contraseña nueva no se confirmo correctamente")
-        }
+          })
+      } else {
+        this.generarError("Contraseña nueva no se confirmo correctamente")
+      }
     }
+  }
 
-snackbarOpen() {
+  snackbarOpen() {
     return this.state.errorMessage !== ""
-}
+  }
 
-snackbarClose = () => {
+  snackbarClose = () => {
     this.setState({
-        errorMessage: ""
+      errorMessage: ""
     })
-}
+  }
 
-handleChange = event => {
+  handleChange = event => {
     this.setState({
-        [event.target.name]: event.target.value
+      [event.target.name]: event.target.value
     })
-}
+  }
 
-generarError(errorMessage) {
+  generarError(errorMessage) {
     this.setState({
-        errorMessage: errorMessage
+      errorMessage: errorMessage
     })
-}
+  }
 
-handleClickOpen() {
+  handleClickOpen() {
     this.setState({
-        open: true,
+      open: true,
     })
-}
+  }
 
-handleClose = () => {
+  handleClose = () => {
     this.props.history.push('/')
     this.setState({
-        open: false,
+      open: false,
     })
-};
+  };
 
-validarConfirmar() {
+  validarConfirmar() {
     return !(this.state.contraseñaActual && this.state.contraseñaNueva && this.state.confirmarContraseñaNueva)
-}
+  }
 
-render() {
+  render() {
     const { errorMessage, open } = this.state
 
     return (
-        <div>
-            <Container component="main" maxWidth="xs" >
-                <div className="contenedorTitulo">
-                    <Typography variant="h3" color="textSecondary">
-                        <PersonIcon fontSize="large"></PersonIcon>
-                    </Typography>
-                    <Typography variant="h5" color="textSecondary">
-                        {"Cambiar contraseña"}
-                    </Typography>
-                </div>
-                <div>
-                    <input type="password" className="inputLogin" placeholder=" Contraseña actual" name="contraseñaActual" onChange={this.handleChange}></input>
-                </div>
-                <div>
-                    <input type="password" className="inputLogin" placeholder=" Contraseña nueva" name="contraseñaNueva" onChange={this.handleChange}></input>
-                </div>
-                <div>
-                    <input type="password" className="inputLogin" placeholder=" Confirmar contraseña nueva" name="confirmarContraseñaNueva" onChange={this.handleChange}></input>
-                </div>
-                <Grid container spacing={16}>
-                    <Grid item xs={5} className="margenBoton">
-                        <button
-                            className="botonLogin"
-                            onClick={() => this.handleClose()}>
-                            Volver
+      <div>
+        <Container component="main" maxWidth="xs" >
+          <div className="contenedorTitulo">
+            <Typography variant="h3" color="textSecondary">
+              <PersonIcon fontSize="large"></PersonIcon>
+            </Typography>
+            <Typography variant="h5" color="textSecondary">
+              {"Cambiar contraseña"}
+            </Typography>
+          </div>
+          <div>
+            <input maxLength="15" type="password" className="inputLogin" placeholder=" Contraseña actual" name="contraseñaActual" onChange={this.handleChange}></input>
+          </div>
+          <div>
+            <input maxLength="15" type="password" className="inputLogin" placeholder=" Contraseña nueva (máximo 15 caracteres)" name="contraseñaNueva" onChange={this.handleChange}></input>
+          </div>
+          <div>
+            <input maxLength="15" type="password" className="inputLogin" placeholder=" Confirmar contraseña nueva" name="confirmarContraseñaNueva" onChange={this.handleChange}></input>
+          </div>
+          <Grid container spacing={0}>
+            <Grid item xs={5} className="margenBoton">
+              <button
+                className="botonLogin"
+                onClick={() => this.handleClose()}>
+                Volver
                             </button>
-                    </Grid>
-                    <Grid item xs={2} > </Grid>
-                    <Grid item xs={5}>
-                        <button
-                            className="botonLogin"
-                            onClick={this.handleEnviar}
-                            disabled={this.validarConfirmar()}>Confirmar</button>
-                    </Grid>
-                </Grid>
-                <SnackBarPersonal mensajeError={errorMessage} abrir={this.snackbarOpen()} cerrar={{ onChange: this.snackbarClose }} variant={"error"} />
-            </Container>
-            <DialogVolver
-                titulo={"Contraseña modificada"}
-                descripcion={"Le confirmamos que su contraseña se modificó exitosamente"}
-                handlers={{ onChange: this.handleClose }}
-                open={open}
-            />
-        </div>
+            </Grid>
+            <Grid item xs={2} > </Grid>
+            <Grid item xs={5}>
+              <button
+                className="botonLogin"
+                onClick={this.handleEnviar}
+                disabled={this.validarConfirmar()}>Confirmar</button>
+            </Grid>
+          </Grid>
+          <SnackBarPersonal mensajeError={errorMessage} abrir={this.snackbarOpen()} cerrar={{ onChange: this.snackbarClose }} variant={"error"} />
+        </Container>
+        <DialogVolver
+          titulo={"Contraseña modificada"}
+          descripcion={"Le confirmamos que su contraseña se modificó exitosamente"}
+          handlers={{ onChange: this.handleClose }}
+          open={open}
+        />
+      </div>
     )
-}
+  }
 }
