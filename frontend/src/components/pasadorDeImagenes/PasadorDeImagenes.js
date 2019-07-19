472299861f8de7cce5586ec2def4ebb3bb214f56
Javier Devoto <javierdevoto96@gmail.com>
2019-07-19 20:52:50 -0300

    Se puede maximizar las imagenes de los items de pedido

--- frontend/src/components/pasadorDeImagenes/PasadorDeImagenes.js
+++ frontend/src/components/pasadorDeImagenes/PasadorDeImagenes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import ImageDialog from '../imageDialog/ImageDialog';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -21,17 +22,27 @@ export default function PasadorDeImagenes(props) {
   var { imagenes } = props;
   const classes = useStyles();
   const imagenesPorDefecto = ["https://media.istockphoto.com/vectors/knife-plate-and-fork-cutlery-vector-retro-illustration-of-a-hand-vector-id871919954", "https://media.istockphoto.com/vectors/knife-plate-and-fork-cutlery-vector-retro-illustration-of-a-hand-vector-id871919954", "https://media.istockphoto.com/vectors/knife-plate-and-fork-cutlery-vector-retro-illustration-of-a-hand-vector-id871919954"];
+  const [open, setOpen] = React.useState(false);
 
+  const openDialog = () => {
+    setOpen(true)
+  }
+
+  const closeDialog = () => {
+    setOpen(false)
+  }
 
   if (imagenes.length === 0) {
     imagenes = imagenesPorDefecto;
   }
+
   return (
     <div className={classes.root}>
       <GridList className={classes.gridList} spacing={0} cols={1}>
         {imagenes.map((imagen) => (
-          <GridListTile key={Math.random()}>
-            <img src={imagen} alt={imagen} />
+          <GridListTile key={Math.random()} >
+            <img onClick={openDialog} src={imagen} alt={imagen} />
+            {open && <ImageDialog abrir={open} imagen={imagen} cerrar={{ onChange: closeDialog }} />}
           </GridListTile>
         ))}
       </GridList>
